2e920c790324bea13224a80ead2df0b9341fa2d9
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-01 11:49:47 +0530

    add one more method to save data in django

--- enroll/views.py
+++ enroll/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from .forms import StudentRegistration
+from .models import User
 # Create your views here.
 def add_show(request):
     if(request.method=='POST'):
         fm=StudentRegistration(request.POST)
         if fm.is_valid():
-            fm.save()
+            # fm.save()  ##(it directly save all data) or,
+            # it save data one by one
+            
+            nm=fm.cleaned_data["name"]
+            em=fm.cleaned_data["email"]
+            pw=fm.cleaned_data["password"]
+            reg=User(name=nm,email=em,password=pw)
+            reg.save()
+            fm=StudentRegistration()
     else:
         fm=StudentRegistration()
          
